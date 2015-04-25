785176084086791c3f9fff67bbf9d7ef1c9fc338
Pere Villega <pvillega@gumtree.com>
2015-04-25 16:59:03 +0100

    calculator

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,7 +2,8 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 ; handlers - given a Ring request returns a response
 (defn greet [req]
@@ -28,6 +29,36 @@
    :body    "Goodbye, World!"
    :headers {}})
 
+(defn about [req]
+  {:status  200
+   :body    "I'm a student of Clojure that is trying things around"
+   :headers {}})
+
+(defn yo [req]
+  (let [name (get-in req [:route-params :name])]
+    {:status  200
+     :body    (str "Yo! " name "!")
+     :headers {}}))
+
+(def ops
+      {"+" +
+       "-" -
+       "*" *
+       ":" /})
+
+(defn calculator [req]
+  (let [a (Integer. (get-in req [:route-params :a]))
+        b (Integer. (get-in req [:route-params :b]))
+        op (get-in req [:route-params :op])
+        f (get ops op)]
+    (if f
+      {:status  200
+       :body    (str "Result: "  (f a b))
+       :headers {}}
+      {:status  404
+       :body    (str "Unknown operator: " op)
+       :headers {}})))
+
 ; middleware - can be stacked (return other middleware) and returns a handler at the end of the chain
 ;(defn mw [hdlr & options]
 ;  (fn [req]
@@ -37,6 +68,10 @@
 (defroutes app
            (GET "/" [] homepage)
            (GET "/goodbye" [] goodbye)
+           (GET "/about" [] about)
+           (GET "/yo/:name" [] yo)
+           (GET "/calc/:a/:op/:b" [] calculator)
+           (GET "/request" [] handle-dump)                  ; does a dump of the header in a nice format
            (not-found "Page not found."))
 
 ; map main to jetty to launch server on given port
